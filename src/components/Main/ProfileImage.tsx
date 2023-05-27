4603b8652bfc335c7d2b62e3cb7666a193027e87
trustmitt <mainmirror14@gmail.com>
2023-05-28 01:09:36 +0900

    feat : introduction 컴포넌트 구현

---
+++ src/components/Main/ProfileImage.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+
+// 자신이 원하는 프로필 이미지 링크로 설정해주세요.
+const PROFILE_IMAGE_LINK =
+  'https://github.com/kohigowild/Gatsby-blog/assets/111720709/a019ae84-9b6e-48b8-92da-e36441b166f9'
+
+const ProfileImageWrapper = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 30px;
+  border-radius: 50%;
+`
+
+const ProfileImage: FunctionComponent = function () {
+  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+}
+
+export default ProfileImage
