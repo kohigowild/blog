4d8d04a1f4a23857ea5c0283d4cd65413abd07b0
trustmitt <mainmirror14@gmail.com>
2023-05-28 13:07:12 +0900

    feat : 게시글 목록 부분 컴포넌트 구현하

---
+++ src/components/Main/PostList.tsx
@@ -0,0 +1,36 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import PostItem from 'components/Main/PostItem'
+
+const POST_ITEM_DATA = {
+  title: 'Post Item Title',
+  date: '2020.01.29.',
+  categories: ['Web', 'Frontend', 'Testing'],
+  summary:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
+  thumbnail:
+    'https://github.com/kohigowild/Gatsby-blog/assets/111720709/a019ae84-9b6e-48b8-92da-e36441b166f9',
+  link: 'https://www.google.co.kr/',
+}
+
+const PostListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  width: 768px;
+  margin: 0 auto;
+  padding: 50px 0 100px;
+`
+
+const PostList: FunctionComponent = function () {
+  return (
+    <PostListWrapper>
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+    </PostListWrapper>
+  )
+}
+
+export default PostList
